1f5882dfb406ca34fad8879e37cef3096dffd94f
Like <like@ekil.sh>
2023-02-28 00:34:23 +0800

    add tauri

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+	let name = 'Like Boss';
+</script>
+
+<h1>Welcome {name}!</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
